5b08ca807677e30ee88f1a0f4d0b6d1beaeb6ee1
Jerome <raoju1580@qq.com>
2018-12-16 23:00:24 +0800

    城市 菜单渲染

--- components/index/menu.vue
+++ components/index/menu.vue
@@ -4,7 +4,7 @@
         @mouseleave="mouseleave"
     >
         <dt>全部分类</dt>
-        <dd v-for="(item ,idx) in menu" 
+        <dd v-for="(item ,idx) in $store.state.home.menu" 
             :key="idx"
             @mouseenter="enter"
         >
@@ -62,7 +62,7 @@
         computed:{
             curdetail(){
                 
-                return this.menu.filter(item => item.type === this.kind)[0];
+                return this.$store.state.home.menu.filter(item => item.type === this.kind)[0];
             }
         },
         methods:{
